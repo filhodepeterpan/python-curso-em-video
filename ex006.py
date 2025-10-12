8a9e318652c5858ddacd1120ac502e51ca2936a5
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-12 17:20:24 -0300

    add_ex6

---
+++ ex006.py
@@ -0,0 +1,8 @@
+# Dobro, Triplo e Raiz
+
+num = int(input("Digite um número: "))
+
+print("Analisando o numéro digitado, temos: ")
+print(f"Dobro: {num * 2}")
+print(f"Triplo: {num * 3}")
+print(f"Raiz quadrada: {(num ** 0.5):.2f}")
