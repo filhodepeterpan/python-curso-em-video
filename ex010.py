786138dc09350d47e019651c0ebf122e88512c5c
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-12 21:45:58 -0300

    add_ex10

---
+++ ex010.py
@@ -0,0 +1,7 @@
+# Conversor de moedas
+
+valorDolar: float = 5.53
+
+dinheiro = float(input("Quanto dinheiro você tem na carteira? R$"))
+
+print(f"Com {dinheiro} você pode comprar US${dinheiro/valorDolar:.2f}.")
