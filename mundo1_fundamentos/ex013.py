a5b8e2fdd7dcba3f16852ca34803a27923368064
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-14 10:29:32 -0300

    add_ex13

---
+++ mundo1_fundamentos/ex013.py
@@ -0,0 +1,7 @@
+# Conversor de Temperatura
+
+temperaturaCelsius = float(input("Informe a temperatura em ºC: "))
+
+temperaturaFahrenheit = temperaturaCelsius * 1.8 + 32
+
+print(f"A temperatura de {temperaturaCelsius}ºC corresponde a {temperaturaFahrenheit}ºF!")
