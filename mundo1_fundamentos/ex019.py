40e008780716ff5132bc201c5e429e9f9f63f6f8
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-15 18:11:44 -0300

    add_ex19

---
+++ mundo1_fundamentos/ex019.py
@@ -0,0 +1,12 @@
+# Sorteando um item na lista
+from random import choice
+
+alunos: any = []
+
+alunos.append(input("Primeiro aluno: "))
+alunos.append(input("Segundo aluno: "))
+alunos.append(input("Terceiro aluno: "))
+alunos.append(input("Quarto aluno: "))
+
+print(f"Alunos participantes: {alunos}\n"
+      f"O aluno sorteado foi: {choice(alunos)}\n")
