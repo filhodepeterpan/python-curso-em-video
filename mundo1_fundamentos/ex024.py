8754b57ac29e22ec946e31ecde8597b0e86b00fb
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-16 19:37:42 -0300

    interpretando novamente o ex 24

--- mundo1_fundamentos/ex024.py
+++ mundo1_fundamentos/ex024.py
@@ -1,9 +1,9 @@
 # Verificando as primeiras letras de um texto
 
 cidade = str(input("Digite o nome de uma cidade: "))
-cidade = cidade.lower().strip().split()
+cidade = cidade.lower().strip()
 
-comecaComSanto : bool = cidade[0] == "santo"
+comecaComSanto : bool = cidade[:5] == "santo"
 resposta = "SIM" if comecaComSanto else "Não..."
 
 print(f"Começa com Santo?: {resposta}")
