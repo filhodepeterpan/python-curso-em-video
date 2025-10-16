1a9ff0071caad37e210e0e1452d1dfe65b394025
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-16 19:30:11 -0300

    add_ex25

---
+++ mundo1_fundamentos/ex025.py
@@ -0,0 +1,9 @@
+# Procurando uma string dentro de outra
+
+nome = str(input("Digite seu nome completo: "))
+nome = nome.lower().strip()
+
+temSilva = "silva" in nome
+resposta = "SIM" if temSilva else "Não..."
+
+print(f"Tem Silva no nome? {resposta}")
