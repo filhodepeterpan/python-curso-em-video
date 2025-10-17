56d8c1ba171062e144fbf600aa268748945bbd6e
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-17 13:38:34 -0300

    melhorando ex 28

--- mundo1_fundamentos/ex028.py
+++ mundo1_fundamentos/ex028.py
@@ -6,8 +6,8 @@ numAleatorio: int = randint(0, 5)
 chute = int(input("Tente advinhar um número entre 0 e 5: "))
 
 if numAleatorio == chute:
-    print(f"Parabéns, você acertou!")
+    print(f"Parabéns, você acertou! O número é {chute}")
 elif chute < 0 or chute > 5:
     print(f"O desafio falhou pois você não digitou um número entre 0 e 5.")
 else:
-    print(f"Infelizmente, você errou... o número é {numAleatorio}")
+    print(f"Infelizmente, você errou... o número é {numAleatorio} e não {chute}.")
