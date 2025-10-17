865eec71b4653cedb56e5b94da3f6bcf27be3da6
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-17 13:42:39 -0300

    adicionando sleep

--- mundo1_fundamentos/ex028.py
+++ mundo1_fundamentos/ex028.py
@@ -1,13 +1,17 @@
 # Jogo da Advinhação v.1.0
 from random import randint
+from time import sleep
 
 numAleatorio: int = randint(0, 5)
 
 chute = int(input("Tente advinhar um número entre 0 e 5: "))
+
+print("Verificando sua resposta...")
+sleep(2)
 
 if numAleatorio == chute:
     print(f"Parabéns, você acertou! O número é {chute}")
 elif chute < 0 or chute > 5:
     print(f"O desafio falhou pois você não digitou um número entre 0 e 5.")
 else:
-    print(f"Infelizmente, você errou... o número é {numAleatorio} e não {chute}.")
+    print(f"Infelizmente, você errou. O número é {numAleatorio} e não {chute}.")
