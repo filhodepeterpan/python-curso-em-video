703f3ef6aa2a6ae3ee854937d58d6d6d824570e3
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-17 13:36:53 -0300

    add_ex28

---
+++ mundo1_fundamentos/ex028.py
@@ -0,0 +1,13 @@
+# Jogo da Advinhação v.1.0
+from random import randint
+
+numAleatorio: int = randint(0, 5)
+
+chute = int(input("Tente advinhar um número entre 0 e 5: "))
+
+if numAleatorio == chute:
+    print(f"Parabéns, você acertou!")
+elif chute < 0 or chute > 5:
+    print(f"O desafio falhou pois você não digitou um número entre 0 e 5.")
+else:
+    print(f"Infelizmente, você errou... o número é {numAleatorio}")
