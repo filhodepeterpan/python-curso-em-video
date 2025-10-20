b9aafe568208b1f7df8afcbd829cb2c08213f2a1
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-20 20:40:52 -0300

    simplificando mensagem

--- mundo2_estruturas_de_controle/ex040.py
+++ mundo2_estruturas_de_controle/ex040.py
@@ -7,18 +7,21 @@ while True:
         nota1 = float(input("Digite sua primeira nota: "))
         nota2 = float(input("Digite sua segunda nota: "))
         resultado : str = ""
+        mensagem : str = ""
 
-        media = (nota1 + nota2) / 2
+        media : float = (nota1 + nota2) / 2
 
         if media >= 7:
             resultado = "APROVADO"
-
+            mensagem = "PARABÉNS"
         elif media >= 5:
             resultado = "EM RECUPERAÇÃO"
+            mensagem = "Cuidado"
         else:
             resultado = "REPROVADO"
+            mensagem = "Que pena"
 
-        print(f"\n{'PARABÉNS' if resultado == 'APROVADO' else 'Que pena' if resultado == 'REPROVADO' else 'Cuidado'}. Você está {resultado}.")
+        print(f"\n{mensagem}. Você está {resultado}.")
         break
 
     except ValueError:
