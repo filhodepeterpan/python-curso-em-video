8c8bd464c1a4f345b3fe7d128cd6693dbc90e565
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-20 19:54:22 -0300

    add_ex37

---
+++ mundo2_estruturas_de_controle/ex037.py
@@ -0,0 +1,34 @@
+# Conversor de Bases Numéricas
+
+num = int(input("Digite um número inteiro: "))
+resultado : str = ""
+tipo_conversao : str = ""
+
+print("Escolha uma das bases para conversão:\n"
+      "[ 1 ] converter para BINÁRIO\n"
+      "[ 2 ] converter para OCTAL\n"
+      "[ 3 ] converter para HEXADECIMAL\n")
+
+while True:
+    try:
+        opcao = int(input("\nSua opção: "))
+
+        if opcao == 1:
+            resultado = bin(num)
+            tipo_conversao = "binário"
+            break
+        elif opcao == 2:
+            resultado = oct(num)
+            tipo_conversao = "octal"
+            break
+        elif opcao == 3:
+            resultado = hex(num)
+            tipo_conversao = "hexadecimal"
+            break
+        else:
+            print("Opção inválida. Escolha apenas 1, 2 ou 3.")
+
+    except ValueError:
+        print("\nDigite apenas números. Escolha 1, 2 ou 3.")
+
+print(f"\n{num} em {tipo_conversao} é {resultado}.")
