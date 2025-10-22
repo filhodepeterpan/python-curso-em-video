c7a7a3f20979967bc9af58901ed4f122be738dde
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-22 14:26:37 -0300

    adicionando sleep

--- mundo2_estruturas_de_controle/ex045.py
+++ mundo2_estruturas_de_controle/ex045.py
@@ -1,6 +1,7 @@
 # Pedra, Papel e Tesoura
 
 from random import randint
+from time import sleep
 
 while True:
     escolha_pc : int = randint(1,3)
@@ -8,7 +9,7 @@ while True:
 
     objeto_pc = "PEDRA" if escolha_pc == 1 else "PAPEL" if escolha_pc == 2 else "TESOURA"
 
-    print("Vamos jogar Pedra, Papel ou Tesoura!Escolha uma opção:\n"
+    print("Vamos jogar Pedra, Papel ou Tesoura!Escolha uma opção:\n\n"
           "[ 1 ] Pedra\n"
           "[ 2 ] Papel\n"
           "[ 3 ] Tesoura\n")
@@ -22,6 +23,13 @@ while True:
                 resultado = "Você venceu!" if escolha_jogador == 3 else "Empatamos!" if escolha_jogador == 2 else "Eu ganhei!"
             if escolha_pc == 3:
                 resultado = "Você venceu!" if escolha_jogador == 1 else "Empatamos!" if escolha_jogador == 3 else "Eu ganhei!"
+
+            print("\nJO...")
+            sleep(1)
+            print("KEN...")
+            sleep(1)
+            print("PÔ!\n")
+            sleep(1)
 
             print(f"Eu escolhi {objeto_pc}. {resultado}")
             break
