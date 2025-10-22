81c88e42235fcc20bd636897e40697410cd2b8c8
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-22 13:24:55 -0300

    add_ex43

---
+++ mundo2_estruturas_de_controle/ex043.py
@@ -0,0 +1,26 @@
+# Índice de Massa Corporal
+
+while True:
+    try:
+        peso = float(input("Digite seu peso em quilos: "))
+        altura = float(input("Digite sua altura em metros: "))
+        classificacao : str = ""
+
+        imc : float = peso / (altura * altura)
+
+        if imc < 18.5:
+            classificacao = "ABAIXO DO PESO"
+        elif imc <= 25:
+            classificacao = "PESO IDEAL"
+        elif imc <= 30:
+            classificacao = "SOBREPESO"
+        elif imc <= 40:
+            classificacao = "OBESIDADE"
+        else:
+            classificacao = "OBESIDADE MÓRBIDA"
+
+        print(f"Seu IMC é de {imc} e sua classificação é: {classificacao}.")
+
+        break
+    except ValueError:
+        print("\nValores inválidos. Tente novamente.")
