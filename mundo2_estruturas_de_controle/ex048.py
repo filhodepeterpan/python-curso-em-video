61bcbd9f0a9b44e3ecde1db5f0bcae6cba485a3d
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-23 19:22:43 -0300

    adicionando contador de impares

--- mundo2_estruturas_de_controle/ex048.py
+++ mundo2_estruturas_de_controle/ex048.py
@@ -1,9 +1,11 @@
 # Soma ímpares múltiplos de três
 
 soma : int = 0
+qt_impares : int = 0
 
 for i in range (1, 501):
     if i % 2 != 0 and i % 3 == 0:
         soma += i
+        qt_impares += 1
 
-print(f"Esta é a soma de todos os números ímpares que são múltiplos de 3 no intervalo entre 1 e 100: {soma}")
+print(f"A soma de todos os {qt_impares} números ímpares que são múltiplos de 3 no intervalo entre 1 e 500 é: {soma}")
