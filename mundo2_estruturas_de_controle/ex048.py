90294110be625bdbea0dc2fd71eca8740fe74b4d
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-23 19:05:57 -0300

    add_ex48

---
+++ mundo2_estruturas_de_controle/ex048.py
@@ -0,0 +1,9 @@
+# Soma ímpares múltiplos de três
+
+soma : int = 0
+
+for i in range (1, 101):
+    if i % 2 != 0 and i % 3 == 0:
+        soma += i
+
+print(f"Esta é a soma de todos os números ímpares que são múltiplos de 3 no intervalo entre 1 e 100: {soma}")
