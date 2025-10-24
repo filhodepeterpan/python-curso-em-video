25ebd8e3630b7ac8556da10cd1cce71bb3840c47
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-24 15:28:15 -0300

    especificando end

--- mundo2_estruturas_de_controle/ex051.py
+++ mundo2_estruturas_de_controle/ex051.py
@@ -7,7 +7,7 @@ while True:
         termo_atual : int = primeiro_termo
 
         for i in range (1, 11):
-            print(f"{termo_atual}", end=" → " if i != 10 else ".")
+            print(f"{termo_atual}", end=" → " if i < 10 else ".")
 
             termo_atual += razao
         break
