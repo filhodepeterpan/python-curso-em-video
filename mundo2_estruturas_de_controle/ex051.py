f9f01676e022b6ca5429656ca3539e79bb145984
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-24 15:27:21 -0300

    melhorando formatacao

--- mundo2_estruturas_de_controle/ex051.py
+++ mundo2_estruturas_de_controle/ex051.py
@@ -7,7 +7,7 @@ while True:
         termo_atual : int = primeiro_termo
 
         for i in range (1, 11):
-            print(f"{i}º termo: {termo_atual}")
+            print(f"{termo_atual}", end=" → " if i != 10 else ".")
 
             termo_atual += razao
         break
