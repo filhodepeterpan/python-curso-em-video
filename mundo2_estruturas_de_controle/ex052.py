18754f37df9973e29fe4d59b9dec6c6d39b2e940
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-24 15:48:17 -0300

    add_ex52

---
+++ mundo2_estruturas_de_controle/ex052.py
@@ -0,0 +1,20 @@
+# Números primos
+
+while True:
+    try:
+        num = int(input("Digite um número inteiro: "))
+        num_primo : bool = True
+
+        if num < 3:
+            num_primo = False
+        else:
+            for i in range (2, num-1):
+                if num % i == 0:
+                    num_primo = False
+                    break
+
+        print(f"\n{num} {'é' if num_primo else 'não é'} um número primo.")
+        break
+
+    except ValueError:
+        print("\nDigite apenas um número inteiro.\n")
