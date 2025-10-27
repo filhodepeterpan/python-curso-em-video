cd89cb5ce78d9656f7a034b36cdefbdab09f3d40
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-27 19:07:43 -0300

    adicionando validacao

--- mundo2_estruturas_de_controle/ex054.py
+++ mundo2_estruturas_de_controle/ex054.py
@@ -5,11 +5,16 @@ ano_atual : int = date.today().year
 maiores_idade : int = 0
 qt_pessoas : int = 7
 
-for i in range (0, qt_pessoas):
-    ano_nascimento = int(input(f"Digite o ano de nascimento da {i+1}ª pessoa: "))
+while True:
+    try:
+        for i in range(0, qt_pessoas):
+            ano_nascimento = int(input(f"Digite o ano de nascimento da {i + 1}ª pessoa: "))
 
-    if ano_nascimento < (ano_atual - 17):
-        maiores_idade += 1
+            if ano_atual - ano_nascimento >= 18:
+                maiores_idade += 1
+        break
+    except ValueError:
+        print("\nVocê digitou um ano inválido. Tente novamente.")
 
 print(f"\nMaiores de idade: {maiores_idade}\n"
       f"Menores de idade: {qt_pessoas - maiores_idade}\n")
