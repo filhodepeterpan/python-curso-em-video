30b625d09e622fc18502e622e7edaf4bef6fac14
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-27 19:14:07 -0300

    adicionando variavel

--- mundo2_estruturas_de_controle/ex054.py
+++ mundo2_estruturas_de_controle/ex054.py
@@ -2,19 +2,20 @@
 from datetime import date
 
 ano_atual : int = date.today().year
-maiores_idade : int = 0
+idade_maioridade : int = 18
+adultos : int = 0
 qt_pessoas : int = 7
 
 while True:
     try:
         for i in range(0, qt_pessoas):
-            ano_nascimento = int(input(f"Digite o ano de nascimento da {i + 1}ª pessoa: "))
+            ano_nascimento = int(input(f"Ano de nascimento da {i + 1}ª pessoa: "))
 
-            if ano_atual - ano_nascimento >= 18:
-                maiores_idade += 1
+            if ano_atual - ano_nascimento >= idade_maioridade:
+                adultos += 1
         break
     except ValueError:
         print("\nVocê digitou um ano inválido. Tente novamente.")
 
-print(f"\nMaiores de idade: {maiores_idade}\n"
-      f"Menores de idade: {qt_pessoas - maiores_idade}\n")
+print(f"\nMaiores de idade: {adultos}\n" 
+      f"Menores de idade: {qt_pessoas - adultos}\n")
