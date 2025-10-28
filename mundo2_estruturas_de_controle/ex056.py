220f0f6846ac2b25e6e3814e167fdfa2b5792ca2
Douglas Soares <douglassoares.cinema@gmail.com>
2025-10-28 14:49:38 -0300

    adicionando exibicao de dados

--- mundo2_estruturas_de_controle/ex056.py
+++ mundo2_estruturas_de_controle/ex056.py
@@ -1,6 +1,6 @@
 # Analisador completo
 
-# ENUNCIADO: Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
+# ENUNCIADO: Desenvolva um programa que leia o nome, idade e gênero de 4 pessoas. No final do programa, mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
 
 from typing import List, Dict
 
@@ -13,14 +13,14 @@ for i in range (0, qt_pessoas):
 
     nome = str(input(f"Nome: ")).strip().capitalize()
     idade = int(input(f"Idade: "))
-    sexo = str(input(f"Sexo: ")).strip().upper()[0]
+    genero = str(input(f"Gênero: ")).strip().upper()[0]
 
     print("\n")
 
     dados.append({
         "nome": nome,
         "idade": idade,
-        "sexo": sexo,
+        "genero": genero,
     })
 
 media_idades : float = sum(p["idade"] for p in dados) / qt_pessoas
@@ -28,16 +28,22 @@ mulheres_menores_20 : int = 0
 homem_mais_velho : str = ""
 idade_homem_mais_velho : int = 0
 
+print(f"\n===== TABELA DE DADOS =====\n"
+      f"{'NOME':<10} | {'IDADE':<5} | {'GENERO':<6}")
+
 for p in dados:
-    if p ["sexo"] == "M":
+    if p ["genero"] == "M":
         if p["idade"] > idade_homem_mais_velho:
             idade_homem_mais_velho = p["idade"]
             homem_mais_velho = p["nome"]
 
-    if p["sexo"] == "F":
+    if p["genero"] == "F":
         if p["idade"] < 20:
             mulheres_menores_20 += 1
 
-print(f"\nMédia de idade: {media_idades:.1f}.\n"
+    print(f"{p["nome"]:<10} | {p["idade"]:<5} | {p["genero"]:<6}")
+
+print(f"\n===== ANÁLISE DE DADOS =====\n"
+      f"Média de idade: {media_idades:.1f}.\n"
       f"Homem mais velho: {homem_mais_velho}\n"
       f"Mulheres com menos de 20: {mulheres_menores_20}\n ")
