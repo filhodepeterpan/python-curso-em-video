7778facdf914cd4a7baba6e2950209552b9549e5
Douglas Soares <douglassoares.cinema@gmail.com>
2025-11-03 19:53:00 -0300

    add_ex58

---
+++ mundo2_estruturas_de_controle/ex058.py
@@ -0,0 +1,19 @@
+# Jogo da Adivinhação v2.0
+
+from random import randint
+from time import sleep
+
+numAleatorio: int = randint(0, 10)
+chute : int | None = None
+tentativas : int = 0
+
+while chute != numAleatorio:
+    chute = int(input("\nTente advinhar um número entre 0 e 10: "))
+
+    if 0 > chute > 5:
+        print(f"\nO desafio falhou pois você não digitou um número entre 0 e 10.")
+    else:
+        print(f"\nO número sorteado é {'maior' if chute < numAleatorio else 'menor'} que {chute}.")
+        tentativas += 1
+
+print(f"\nParabéns, o número é {chute}! Você acertou com {tentativas} {'tentativas' if tentativas > 1 else 'tentativa'}.")
